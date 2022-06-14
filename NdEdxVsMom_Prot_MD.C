8b7af458a8fbd1197f713cba8a3fb4854035bdf9
mndbr <m.dbr@outlook.com>
2022-06-14 13:46:25 +0200

    slice for pion added - still need tuning

--- NdEdxVsMom_Prot_MD.C
+++ NdEdxVsMom_Prot_MD.C
@@ -5,13 +5,17 @@ TF1* gBethe_Bloch_p;
 TF1* gBethe_Bloch_pi;
 TF1* gBethe_Bloch_k;
 TF1* GetGaussFitPol(TH1D* h, Float_t p, const Double_t* params);
+TF1* GetGaussFitPol_pi(TH1D* h, Float_t p, const Double_t* params);
+
 void TPstyle();
 void DrawSlice(TH1D* h,TCanvas* canv);
-void DyVsY2(Int_t nSlicesInit, TH2F* h2, string particle);
+void DyVsY2(Int_t nSlicesInit, TH2F* h2);
+void DyVsY3(Int_t nSlicesInit, TH2F* h2);
+
 Double_t BetheBloch(Double_t *x, Double_t *par);
 
 //___________MAIN FUNCTION_______________________________________________________________________//
-void FitSlices(Int_t runid=1234, Int_t apver=23, Int_t nSlices=15, Int_t saveCalibParams=0, string particle="proton")
+void FitSlices(Int_t runid=1234, Int_t apver=23, Int_t nSlices=15, Int_t saveCalibParams=0)
 {
 
   gStyle->SetOptStat(0);
@@ -86,26 +90,34 @@ void FitSlices(Int_t runid=1234, Int_t apver=23, Int_t nSlices=15, Int_t saveCal
   TH2F* hrplot = (TH2F*)hr->Clone("hrplot");
 
 // Slicing function for pions/protons
-  DyVsY2(nSlices,hr,particle);
-  
-  TCanvas* c1 = new TCanvas ("dEdx","dEdx",100,100,650,560);
+  DyVsY2(nSlices,hr);
+  DyVsY3(nSlices,hr_pi);
+
+
+    TCanvas* c1 = new TCanvas ("dEdx","dEdx",100,100,650,560);
 
   c1->Divide(1,1);
   c1->cd(1);
 
-  TList* list = hr->GetListOfFunctions();
+  TList* list_p = hr->GetListOfFunctions();
+  TList* list_pi = hr_pi->GetListOfFunctions();
+
   TMultiGraph* mg1 = new TMultiGraph();
   
-  TGraphErrors* mean     = (TGraphErrors*)list->FindObject(Form("Mean"));
-  //TGraphErrors* Sigma    = (TGraphErrors*)list->FindObject(Form("Sigma"));
+  TGraphErrors* mean_p     = (TGraphErrors*)list_p->FindObject(Form("Mean"));
+  TGraphErrors* mean_pi     = (TGraphErrors*)list_pi->FindObject(Form("Mean"));
+
+    //TGraphErrors* Sigma    = (TGraphErrors*)list->FindObject(Form("Sigma"));
   //TGraphErrors* SigmaVsMean    = (TGraphErrors*)list->FindObject(Form("SigmaVsMean"));
   //TGraphErrors* SigmaVsBeta    = (TGraphErrors*)list->FindObject(Form("SigmaVsBeta"));
   //TGraphErrors* SigmaVsBetaProt    = (TGraphErrors*)list->FindObject(Form("SigmaVsBetaProt"));
   //TGraphErrors* meanPsigma     = (TGraphErrors*)list->FindObject(Form("MeanPSigma"));
   //TGraphErrors* meanMsigma     = (TGraphErrors*)list->FindObject(Form("MeanMSigma"));
 
-  mg1->Add(mean);
-  //mg1->Add(SigmaVsBeta);
+  mg1->Add(mean_p);
+  mg1->Add(mean_pi);
+
+    //mg1->Add(SigmaVsBeta);
   //mg1->Add(SigmaVsBetaProt);
   //mg1->Add(SigmaVsMean);
   //mg1->Add(Sigma);
@@ -114,8 +126,10 @@ void FitSlices(Int_t runid=1234, Int_t apver=23, Int_t nSlices=15, Int_t saveCal
   hrplot->GetXaxis()->SetTitle("log(p) [GeV/c]");
   hrplot->GetYaxis()->SetTitle("dEdx [arbitrary]");
 
-  hrplot->GetListOfFunctions()->Add(mean);
-  //hrplot->GetListOfFunctions()->Add(SigmaVsMean);
+  hrplot->GetListOfFunctions()->Add(mean_p);
+    hrplot->GetListOfFunctions()->Add(mean_pi);
+
+    //hrplot->GetListOfFunctions()->Add(SigmaVsMean);
   //hrplot->GetListOfFunctions()->Add(meanMsigma);
   
 
@@ -153,7 +167,7 @@ void FitSlices(Int_t runid=1234, Int_t apver=23, Int_t nSlices=15, Int_t saveCal
 }
 
 //_______________________________________________________________________
-void DyVsY2(Int_t nSlicesInit, TH2F* h2, string particle)
+void DyVsY2(Int_t nSlicesInit, TH2F* h2)
 {
   // this is general method to perform pid on m2 versus momentum
   // plot. The results are saved in file at path location.
@@ -373,6 +387,227 @@ void DyVsY2(Int_t nSlicesInit, TH2F* h2, string particle)
       cout << "functions added" << endl;
 }
 
+//_______________________________________________________________________
+void DyVsY3(Int_t nSlicesInit, TH2F* h2)
+{
+    // this is general method to perform pid on m2 versus momentum
+    // plot. The results are saved in file at path location.
+    // path should point to the particular (Tof/Rich) selector
+    // pid directory
+
+    TH1D *slice[100];
+
+    Float_t pmin = gPmin;
+    Float_t pmax = gPmax;
+
+
+    Float_t pstepInit = (pmax - pmin) / nSlicesInit;
+    cout << "initial hbw/2 is " << pstepInit / 2. << endl;
+
+    Float_t pstep = pstepInit;
+
+    //Float_t pL = pmin;
+    //Float_t pU;
+    Int_t nSlices = 0;
+
+    Float_t pstep_array[] = {3.3, 3.3, 3.3, 3.3, 3.3, 3.3, 3.3, 3.3, 4, 5, 6};
+    nSlicesInit = sizeof(pstep_array) / 4;
+
+    Float_t pL;
+    Float_t pU;
+
+    for (Int_t i = 0; i < nSlicesInit; i++) {
+        //if(i>0)pL = pU;
+        // if(pL>7)pstep = 2*pstepInit;
+        //pU = pL + pstep_array[i];
+
+        cout << "DyVsY2: number of slices is: " << nSlicesInit << endl;
+        //pU = pmin + pstep * (i+1);
+        pL = pmin + pstep * i;
+        pU = pmin + pstep * (i + 1);
+
+        Int_t binL = h2->GetXaxis()->FindBin(pL);
+        Int_t binU = h2->GetXaxis()->FindBin(pU);
+        Float_t p = 0.5 * (pL + pU);
+        // if(p>pmax)break;
+        nSlices++;
+        slice[i] = (TH1D *) h2->ProjectionY(Form("slice%d", i), binL, binU);
+        slice[i]->SetTitle(Form("p range: %f - %f GeV/c", pL, pU));
+        slice[i]->Sumw2();
+        //slice[i]->Rebin(binsToMerge);
+        //slice[i]->Scale(1./(Float_t)binsToMerge);
+        cout << "i=" << i << "  pL=" << pL << "   pU=" << pU << " max=" << slice[i]->GetMaximum() << endl;
+        if (slice[i]->GetMaximum() < 3) {
+            slice[i]->Rebin(2);
+            //slice[i]->Scale(1./(Float_t)2);
+        }
+    }
+
+    pmax = pU;
+
+    cout << "Starting initial fit of slices" << endl;
+
+    Float_t momPart[100];
+    Float_t momPartErr[100];
+    Float_t meanPart[100];
+    Float_t meanPartErr[100];
+    Float_t sigmaPart[100];
+    Float_t sigmaPartProt[100];
+    Float_t sigmaPartErr[100];
+    Float_t betaPart[100];
+    Float_t betaPartProt[100];
+    Float_t betaPartErr[100];
+
+    pL = pmin;
+    pstep = pstepInit;
+
+    for (Int_t i = 0; i < nSlicesInit; i++) {
+        Float_t pL = pmin + pstep * i;
+        Float_t pU = pmin + pstep * (i + 1);
+        //if(i>0)pL = pU;
+        // if(pL>7)pstep = 2*pstepInit;
+        //pU = pL + pstep_array[i];
+        Int_t binL = h2->GetXaxis()->FindBin(pL);
+        Int_t binU = h2->GetXaxis()->FindBin(pU);
+        Float_t p = 0.5 * (pL + pU);
+        // if(p>pmax)break;
+
+        TH1D *h = slice[i];
+
+        // najwieksza lp zliczen:
+        Int_t maxBin = h->GetMaximumBin();
+        // max w binie
+        Float_t cons = h->GetMaximum();
+        // wartosc dla srodka binu
+        Float_t mean = h->GetBinCenter(maxBin);
+        Float_t sigm = 1.3;
+
+        // ewentualne tło
+        Float_t consB = h->GetBinContent(maxBin / 2);
+        Float_t meanB = h->GetBinCenter(maxBin);
+        Float_t sigmB = 100 * sigm;
+
+        // do fitu parametry
+        Double_t params[6];
+
+        params[0] = cons;
+        params[1] = mean;
+        params[2] = sigm;
+        params[3] = consB;
+        params[4] = meanB;
+        params[5] = sigmB;
+
+        cout << "i=" << i << "  pL=" << pL << "   pU=" << pU << "  p=" << 0.5 * (pL + pU) << endl;
+
+        TF1 *func = GetGaussFitPol_pi(slice[i], 0.5 * (pL + pU), params);
+
+        Bool_t drawSlices = kTRUE;
+        if (drawSlices) {
+            cnv[i] = new TCanvas(Form("slice%d", i), Form("slice%d", i), 5 * i, 5 * i, 400, 560);
+            cnv[i]->SetTopMargin(0.02);
+            cnv[i]->SetRightMargin(0.05);
+            cnv[i]->SetLeftMargin(0.16);
+            cnv[i]->SetBottomMargin(0.15);
+            cnv[i]->Divide(1, 1);
+
+            DrawSlice(slice[i], cnv[i]);
+        }
+
+
+        momPart[i] = p;
+        momPartErr[i] = p * 0.005; // this should depend on field settings
+        meanPart[i] = func->GetParameter(1); // to co powinien dawac Bethe Bloch
+        meanPartErr[i] = TMath::Abs(func->GetParError(1));
+        sigmaPart[i] = TMath::Abs(func->GetParameter(2));
+        sigmaPartErr[i] = TMath::Abs(func->GetParError(2));
+        betaPart[i] = p / TMath::Sqrt(p * p + 0.880354496);
+        betaPartErr[i] = betaPart[i] * 0.000001;
+
+    }
+
+
+    h2->GetListOfFunctions()->Clear();
+
+    TGraphErrors *Mean = new TGraphErrors(nSlices, momPart, meanPart, momPartErr, meanPartErr);
+    TGraphErrors *Sigma = new TGraphErrors(nSlices, momPart, sigmaPart, momPartErr, sigmaPartErr);
+    TGraphErrors *SigmaVsMean = new TGraphErrors(nSlices, meanPart, sigmaPart, meanPartErr, sigmaPartErr);
+    TGraphErrors *SigmaVsBeta = new TGraphErrors(nSlices, betaPart, sigmaPart, betaPartErr, sigmaPartErr);
+    TGraphErrors *SigmaVsBetaProt = new TGraphErrors(nSlices, betaPartProt, sigmaPartProt, betaPartErr, sigmaPartErr);
+
+    Mean->SetName(Form("Mean"));
+    Sigma->SetName(Form("Sigma"));
+    SigmaVsMean->SetName("SigmaVsMean");
+    SigmaVsBeta->SetName("SigmaVsBeta");
+    SigmaVsBetaProt->SetName("SigmaVsBetaProt");
+
+    Mean->SetMarkerStyle(20);
+    Mean->SetMarkerSize(0.05);
+    Mean->SetMarkerColor(2);
+    Mean->SetLineColor(2);
+    Mean->SetLineWidth(3);
+
+    Sigma->SetMarkerStyle(20);
+    Sigma->SetMarkerSize(0.05);
+    Sigma->SetMarkerColor(4);
+    Sigma->SetLineColor(4);
+    Sigma->SetLineWidth(3);
+
+    SigmaVsMean->SetMarkerStyle(20);
+    SigmaVsMean->SetMarkerSize(0.05);
+    SigmaVsMean->SetMarkerColor(4);
+    SigmaVsMean->SetLineColor(4);
+    SigmaVsMean->SetLineWidth(3);
+
+    SigmaVsBeta->SetMarkerStyle(20);
+    SigmaVsBeta->SetMarkerSize(0.05);
+    SigmaVsBeta->SetMarkerColor(6);
+    SigmaVsBeta->SetLineColor(4);
+    SigmaVsBeta->SetLineWidth(3);
+
+    SigmaVsBetaProt->SetMarkerStyle(20);
+    SigmaVsBetaProt->SetMarkerSize(0.05);
+    SigmaVsBetaProt->SetMarkerColor(2);
+    SigmaVsBetaProt->SetLineColor(2);
+    SigmaVsBetaProt->SetLineWidth(3);
+
+    // PROTON
+
+    TF1 *meanFit_pion = new TF1("Bethe Bloch p", BetheBloch, -0.5, 3, 5);
+    meanFit_pion->SetParNames("X0", "X1", "miu", "s");
+    meanFit_pion->SetLineColor(3);
+    meanFit_pion->SetLineWidth(4);
+    meanFit_pion->SetParameters(gBethe_Bloch_pi->GetParameter(0), gBethe_Bloch_pi->GetParameter(1),
+                                gBethe_Bloch_pi->GetParameter(2), gBethe_Bloch_pi->GetParameter(3),
+                                gBethe_Bloch_pi->GetParameter(4));
+    meanFit_pion->FixParameter(4, gBethe_Bloch_pi->GetParameter(4));
+    Mean->Fit(meanFit_pion, "w", "", pmin - 0.1, pmax + 0.1);
+
+    // PION
+    // Te piony są z dopasowania protonu
+    // parametry z dopasowanai protonu powinny opisywac piony
+//      TF1 *meanFit_pion = new TF1("Bethe Bloch pi", BetheBloch, -0.5, 3, 5);
+//      meanFit_pion->SetParNames("X0", "X1", "miu", "s");
+//      meanFit_pion->SetLineColor(3);
+//      meanFit_pion->SetLineWidth(4);
+//      meanFit_pion->SetParameters(meanFit_prot->GetParameter(0), meanFit_prot->GetParameter(1),
+//                                  meanFit_prot->GetParameter(2), meanFit_prot->GetParameter(3),
+//                                  meanFit_prot->GetParameter(4));
+//      meanFit_pion->FixParameter(4, gBethe_Bloch_pi->GetParameter(4));
+
+
+
+    // Mean->GetListOfFunctions()->Add(meanFit_pion);
+    Mean->GetListOfFunctions()->Add(meanFit_pion);
+
+    h2->GetListOfFunctions()->Add(Mean);
+    // h2->GetListOfFunctions()->Add(Sigma);
+    // h2->GetListOfFunctions()->Add(SigmaVsMean);
+    // h2->GetListOfFunctions()->Add(SigmaVsBeta);
+    // h2->GetListOfFunctions()->Add(SigmaVsBetaProt);
+
+    cout << "functions added" << endl;
+}
+
 //___________________________________________________________________________
 void DrawSlice(TH1D* h,TCanvas* canv)
 {
@@ -416,14 +651,12 @@ TF1* GetGaussFitPol(TH1D* h, Float_t p, const Double_t* params)
   func->SetParameters(params);
 
   func->SetParLimits(0,1,500);
-  func->SetParLimits(1,gBethe_Bloch_p->Eval(p)-3*dd, gBethe_Bloch_p->Eval(p)+3*dd);
-  func->SetParLimits(2,sig-0.015,sig+0.015);
-  cout<<"gBethe_Bloch_p->Eval(p) : "<<gBethe_Bloch_p->Eval(p)<<" p="<<p<<endl;
-  //cout<<gBethe_Bloch_p->Eval(p)<<endl;
 
-  //func->SetParLimits(3,0,200);
-  //func->SetParLimits(4,-0.5,0.5);
-  //func->SetParLimits(5,-0.3,0);
+    func->SetParLimits(1,gBethe_Bloch_p->Eval(p)-3*dd, gBethe_Bloch_p->Eval(p)+3*dd);
+    func->SetParLimits(2,sig-0.015,sig+0.015);
+    cout<<"gBethe_Bloch_p->Eval(p) : "<<gBethe_Bloch_p->Eval(p)<<" p="<<p<<endl;
+
+
   func->FixParameter(3,0);
   func->FixParameter(4,0);
   func->FixParameter(5,0);
@@ -451,6 +684,67 @@ TF1* GetGaussFitPol(TH1D* h, Float_t p, const Double_t* params)
   
   return func;
 }
+
+//______________________________________________________________
+TF1* GetGaussFitPol_pi(TH1D* h, Float_t p, const Double_t* params)
+{
+// h - histograms to fit
+// params - pointer to list of parameters
+// n - number of Gaussians to consider
+// The method create function fit it to histogram and return.
+
+// Rozkład Gaussa
+TString g1 = "[0]*exp(-0.5*((x-[1])/[2])^2)";
+
+// Polynomial pierwszy wyraz liniowy, drugi kwadratowy ale względem 1.13
+// parabola, ktrora ma wierzchołek w 1.13
+// cala funkcja to jest g1 + g2
+TString g2 = "[3] + [4]*(x-1.13) + [5]*(x-1.13)*(x-1.13)";
+TString name = h->GetName();
+name.Append("_Fit");
+cout<<"name: "<<name.Data()<<endl;
+TF1* func = new TF1(name.Data(),Form("%s + %s",g1.Data(),g2.Data()),0.7,1.8);
+
+Float_t sig = 0.054;
+Float_t dd = 0.01;
+
+func->SetParameters(params);
+
+func->SetParLimits(0,1,500);
+
+func->SetParLimits(1,gBethe_Bloch_pi->Eval(p)-3*dd, gBethe_Bloch_pi->Eval(p)+3*dd);
+func->SetParLimits(2,sig-0.015,sig+0.015);
+cout<<"gBethe_Bloch_p->Eval(p) : "<<gBethe_Bloch_pi->Eval(p)<<" p="<<p<<endl;
+
+
+func->FixParameter(3,0);
+func->FixParameter(4,0);
+func->FixParameter(5,0);
+
+//func->SetParNames("const","mean","sigma","constB","meanB","sigmaB");
+func->SetParNames("const","mean","sigma","p0","p1","p2");
+
+
+//limits on mean, sigma and constant
+
+Float_t rmin = 0.7;
+Float_t rmax =  1.8;
+
+h->Rebin(4);
+//h->Scale(0.25);
+
+h->Fit(func,"w0","",rmin,rmax);
+
+//func->SetParLimits(1,func->GetParameter(1)-1*dd, func->GetParameter(1)+1*dd);
+//func->SetParLimits(2,func->GetParameter(2)-0.005,func->GetParameter(2)+0.035);
+
+h->Fit(func,"w0","",rmin,rmax);
+
+h->GetListOfFunctions()->Add(func);
+
+return func;
+}
+
 
 //__________________________________________________________________________
 Double_t BetheBloch(Double_t *x, Double_t *par) 
